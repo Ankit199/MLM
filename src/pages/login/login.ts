cc191b42f8b533ee9ef0b96a712dbcaccf5c732c
villagerboy <ankitshukla1992@outlook.com>
2019-03-14 19:35:21 +0530

    logout added 1

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -29,7 +29,7 @@ export class LoginPage {
     isRemember:true
   };
   constructor(
-    private toastr: ToastrService,
+   
     public events: Events,
     public setting: SettingsProvider,
     public comn: CommonfunctionProvider,
